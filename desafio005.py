a44d67736e68f86139627b0538a8c08ecc73cd3e
marcelodevzs <telartsnasestradas@gmail.com>
2025-12-07 12:02:51 -0300

    Desafio 005

---
+++ desafio005.py
@@ -0,0 +1,11 @@
+print('====== DESAFIO 005 ======')
+print()
+print('Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.')
+print()
+n = int(input('Digite um número : '))
+n1 = n - 1
+n2 = n + 1
+print('Antecessor : {}'.format(n1))
+print('Sucessor : {}'.format(n2))
+print()
+print('Criado por : Marcelo Augusto')
