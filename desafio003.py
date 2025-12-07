463fa174f1ce932b5cba8e0da729e77490b90b5a
marcelodevzs <telartsnasestradas@gmail.com>
2025-12-07 11:52:07 -0300

    Desafio 003

---
+++ desafio003.py
@@ -0,0 +1,10 @@
+print('====== DESAFIO 003 ======')
+print()
+print('Somando 2 Números.')
+print()
+n1 = int(input('Digite um número : '))
+n2 = int(input('Digite outro número : '))
+s = n1 + n2
+print('A soma entre {} e {} vale {} !'.format(n1, n2, s))
+print()
+print('Criado por : Marcelo Augusto')
